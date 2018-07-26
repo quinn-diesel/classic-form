86391c239807b0fcaae71b5d7c3bc9248be3b5ea
QUINN STOBBS <quinn@atlasagency.com.au>
2018-07-26 20:04:27 +1000

    creating local storage object

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 console.log('linked');
 
-$( "#form" ).submit((e) => { 
+$( "#form" ).submit(function( event ) { 
 
-
+    
     const fields = {};
-    $("#form").find(":input").each(() => {
+    $("#form").find(":input").each(function() {
         fields[this.name] = $(this).val();
     });
-
     const obj = {fields: fields};
-
     console.log(obj);
+
+    $("select.time").change(function(){
+        const timeValue = $(".time option:selected").val();
+        console.log(timeValue);
+        event.preventDefault();
+    });
+    
+
     console.log('clicked');
     event.preventDefault();
   });
