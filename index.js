a969b9b19d5e851dc35beb80bef8462743e334c3
QUINN STOBBS <quinn@atlasagency.com.au>
2018-07-26 19:48:40 +1000

    adding in the js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('linked');
+
+$( "#form" ).submit((e) => { 
+
+
+    const fields = {};
+    $("#form").find(":input").each(() => {
+        fields[this.name] = $(this).val();
+    });
+
+    const obj = {fields: fields};
+
+    console.log(obj);
+    console.log('clicked');
+    event.preventDefault();
+  });
+
+ 
